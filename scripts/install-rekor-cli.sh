e8cd5374dd4de5ba439f38c9ed6c279f1731b319
cpanato <ctadeu@gmail.com>
2022-07-25 11:24:54 +0200

    install once rekor-cli
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- scripts/install-rekor-cli.sh
+++ scripts/install-rekor-cli.sh
@@ -30,14 +30,6 @@ install_rekor() {
     exit 0
   fi
 
-  default_version='v0.9.1'
-  default_linux_amd64_sha='1bebeaf3d8fbd16841a6f9cf86312085b4eb331075d17978af2fd221ce361eb3'
-  default_linux_arm_sha='1fe49488ff6057c5d06f0fe8a383e5193319d108bf3d7955c58490ab1e74570b'
-  default_linux_arm64_sha='d0d3c513baa89ab256cacebe6d33e5e765ecc493bc672aa213d5a87679a1aa1d'
-  default_darwin_amd64_sha='3d06cf86d726f625f94d16613b671b0e280c349662bf50e2a7a262d09d993849'
-  default_darwin_arm64_sha='10924422c9e48a35fba089039612c96bcb38b5e7799191b2e1119fd57a94fddf'
-  default_windows_amd64_sha='f2f698d9210e85cd8264a45faec9afcb7907dd02614bdde96a5004bd5c1816e0'
-
   trap "popd >/dev/null" EXIT
 
   pushd "$(eval echo "$INSTALL_DIR")" > /dev/null
@@ -46,20 +38,14 @@ install_rekor() {
     Linux)
       case $ARCH in
         X64)
-          default_filename='rekor-cli-linux-amd64'
-          default_sha=${default_linux_amd64_sha}
           desired_rekor_cli_filename='rekor-cli-linux-amd64'
           ;;
 
         ARM)
-          default_filename='rekor-cli-linux-arm'
-          default_sha=${default_linux_arm_sha}
           desired_rekor_cli_filename='rekor-cli-linux-arm'
           ;;
 
         ARM64)
-          default_filename='rekor-cli-linux-arm64'
-          default_sha=${default_linux_arm64_sha}
           desired_rekor_cli_filename='rekor-cli-linux-amd64'
           ;;
 
@@ -73,14 +59,10 @@ install_rekor() {
     macOS)
       case $ARCH in
         X64)
-          default_filename='rekor-cli-darwin-amd64'
-          default_sha=${default_darwin_amd64_sha}
           desired_rekor_cli_filename='rekor-cli-darwin-amd64'
           ;;
 
         ARM64)
-          default_filename='rekor-cli-darwin-arm64'
-          default_sha=${default_darwin_arm64_sha}
           desired_rekor_cli_filename='rekor-cli-darwin-arm64'
           ;;
 
@@ -94,8 +76,6 @@ install_rekor() {
     Windows)
       case $ARCH in
         X64)
-          default_filename='rekor-cli-windows-amd64.exe'
-          default_sha=${default_windows_amd64_sha}
           desired_rekor_cli_filename='rekor-cli-windows-amd64.exe'
           ;;
         *)
@@ -109,23 +89,6 @@ install_rekor() {
       exit 1
       ;;
   esac
-
-  expected_default_version_digest=${default_sha}
-  log_info "Downloading default version '${default_version}' of rekor-cli to verify version to be installed...\n      https://github.com/sigstore/rekor/releases/download/${default_version}/${default_filename}"
-  curl -sL https://github.com/sigstore/rekor/releases/download/${default_version}/${default_filename} -o rekor-cli
-  shadefault=$(shaprog rekor-cli);
-  if [[ $shadefault != "${expected_default_version_digest}" ]]; then
-    log_error "Unable to validate rekor-cli version: '$REKOR_VERSION'"
-    exit 1
-  fi
-  chmod +x rekor-cli
-
-  # If the default and specified `rekor-cli` releases are the same, we're done.
-  if [[ $REKOR_VERSION == "${default_version}" ]]; then
-    COSIGN_EXPERIMENTAL=1 cosign verify-blob --signature https://github.com/sigstore/rekor/releases/download/"$REKOR_VERSION"/${desired_rekor_cli_filename}-keyless.sig rekor-cli
-    log_info "default version successfully verified and matches requested version so nothing else to do"
-    exit 0
-  fi
 
   semver='^v([0-9]+\.){0,2}(\*|[0-9]+)$'
   if [[ $REKOR_VERSION =~ $semver ]]; then
